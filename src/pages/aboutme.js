323323f393f4363d405df31e62e2cea7db372f4a
chiouonthis <jchiou1984@gmail.com>
2018-12-03 23:48:50 -0500

    Fixed link for SG reviews/about

--- src/pages/aboutme.js
+++ src/pages/aboutme.js
@@ -5,7 +5,7 @@ const IndexPage = () => (
   <Layout>
     <h1>About me</h1>
     <ul>
-        <li>Alumnus of <a href="https://www.coursereport.com/schools/software-guild#/outcomes">The Software Guild</a> in Akron, Ohio</li>
+        <li>Alumnus of <a href="https://www.coursereport.com/schools/software-guild#/about">The Software Guild</a> in Akron, Ohio</li>
         <li>Obtained a B.A. in English and Communication from <a href="https://www.lakeforest.edu/">Lake Forest College</a> in Lake Forest, IL</li>
         <li>Currently based in <a href="https://www.youtube.com/watch?v=oKDjis1fg8E">Northeast Ohio</a></li>
         <li>Happily married with two kids :)</li>
